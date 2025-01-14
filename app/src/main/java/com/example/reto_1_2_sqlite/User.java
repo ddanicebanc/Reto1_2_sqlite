a4ec6f46a42251c404b5fa9bccef8bf9b9a76bbf
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-14 10:25:04 +0100

    Added Serializable interface to the class

--- app/src/main/java/com/example/reto_1_2_sqlite/User.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/User.java
@@ -1,6 +1,8 @@
 package com.example.reto_1_2_sqlite;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String name;
     private int id;
 
