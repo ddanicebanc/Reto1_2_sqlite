78d2d39bcea40fe851533e5c219e5b6fa03b7403
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-14 10:41:51 +0100

    Added user control and linked calendar layout

--- app/src/main/java/com/example/reto_1_2_sqlite/CalendarActivity.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/CalendarActivity.java
@@ -7,8 +7,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CalendarActivity extends AppCompatActivity {
+    User user;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.layout_pantalla_calendario);
+
+        user = (User) getIntent().getSerializableExtra("cUser");
+
+
     }
 }
