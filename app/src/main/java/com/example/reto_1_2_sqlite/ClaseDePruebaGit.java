738a9bb54e3f6e9c16e5e1133a09b56dc3ff6de3
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-15 09:27:12 +0100

    Added ClaseDePruebaGit to project

---
+++ app/src/main/java/com/example/reto_1_2_sqlite/ClaseDePruebaGit.java
@@ -0,0 +1,14 @@
+package com.example.reto_1_2_sqlite;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClaseDePruebaGit extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
