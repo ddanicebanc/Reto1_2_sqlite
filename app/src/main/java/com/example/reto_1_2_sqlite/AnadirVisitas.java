2d990cb7455699d217ae23c4bec0a6829c2913c7
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-17 12:39:26 +0100

    Refactored code: made variables static for ease of use

--- app/src/main/java/com/example/reto_1_2_sqlite/AnadirVisitas.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/AnadirVisitas.java
@@ -2,6 +2,7 @@ package com.example.reto_1_2_sqlite;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,20 +11,26 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class AnadirVisitas extends AppCompatActivity implements Serializable {
+    public static String nombre, fecha, direccion;
 
     private EditText editNombreVista, editFecha, editDireccion;
     private Button guardarButton;
+    DBHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.layout_pantalla_anadir_visitas);
+
+        User user = (User) getIntent().getSerializableExtra("cUser");
+        handler = new DBHandler(AnadirVisitas.this);
 
         // Inicializar los campos y el botón
         editNombreVista = findViewById(R.id.editNombreVista);
@@ -32,36 +39,59 @@ public class AnadirVisitas extends AppCompatActivity implements Serializable {
         guardarButton = findViewById(R.id.Guardar);
 
         // Configurar el listener del botón
-        guardarButton.setOnClickListener(v -> validarCampos());
+        guardarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validarCampos()) {
+                    ArrayList<String> columnas = new ArrayList<>();
+                    ArrayList<String> datos = new ArrayList<>();
+
+                    columnas.add("usuarioId");
+                    datos.add(String.valueOf(user.getId()));
+
+                    columnas.add("partnerId");
+                    datos.add(String.valueOf(user.getId()));
+
+                    columnas.add("fechaVisita");
+                    datos.add("fecha");
+
+                    columnas.add("direccion");
+                    datos.add("direccion");
+
+                    handler.insertData("visitas", columnas, datos);
+                }
+            }
+        });
+
+        //Llamada al botón de guardado
     }
 
-    private void validarCampos() {
-        String nombre = editNombreVista.getText().toString().trim();
-        String fecha = editFecha.getText().toString().trim();
-        String direccion = editDireccion.getText().toString().trim();
+    private boolean validarCampos() {
+        //TODO Cambiar toast por AlertDialog
+        nombre = editNombreVista.getText().toString().trim();
+        fecha = editFecha.getText().toString().trim();
+        direccion = editDireccion.getText().toString().trim();
+        boolean validado = true;
 
         if (TextUtils.isEmpty(nombre) || TextUtils.isEmpty(fecha) || TextUtils.isEmpty(direccion)) {
             Toast.makeText(this, "Todos los campos deben estar llenos", Toast.LENGTH_LONG).show();
-            return;
+            validado = false;
+        } else {
+            //Query para comprobar que el nombre del partner introducido existe
+            if (!handler.searchByName("partners", "nombre", nombre)) {
+                validado = false;
+            }
+            if (!isFechaValida(fecha)) {
+                Toast.makeText(this, "El campo 'Fecha' debe contener una fecha válida (dd/mm/yyyy)", Toast.LENGTH_LONG).show();
+                validado = false;
+            }
+            if (!direccion.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9 ]+")) {
+                Toast.makeText(this, "El campo 'Dirección' solo puede contener letras y números", Toast.LENGTH_LONG).show();
+                validado = false;
+            }
         }
 
-        if (!nombre.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+")) {
-            Toast.makeText(this, "El campo 'Nombre' solo puede contener letras", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if (!isFechaValida(fecha)) {
-            Toast.makeText(this, "El campo 'Fecha' debe contener una fecha válida (dd/mm/yyyy)", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if (!direccion.matches("[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9 ]+")) {
-            Toast.makeText(this, "El campo 'Dirección' solo puede contener letras y números", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        // Si pasa todas las validaciones
-        Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_LONG).show();
+        return validado;
     }
 
     private boolean isFechaValida(String fecha) {
