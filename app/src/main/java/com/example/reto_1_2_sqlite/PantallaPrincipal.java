af862d014e32a42fbcd157a6dedbbf0a887d73cc
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-17 08:29:41 +0100

    Fixed onCreate method to visualize activity layout

--- app/src/main/java/com/example/reto_1_2_sqlite/PantallaPrincipal.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/PantallaPrincipal.java
@@ -15,8 +15,8 @@ public class PantallaPrincipal extends AppCompatActivity implements Serializable
     ImageButton btnCalendar;
     User user;
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_pantalla_principal);
 
         user = (User) getIntent().getSerializableExtra("cUser");
