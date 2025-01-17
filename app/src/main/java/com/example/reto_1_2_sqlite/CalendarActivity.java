9804fdb5348ada2aa25c7504c3b0852b74415334
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-17 08:35:16 +0100

    Added recyclerView functionality to activity

--- app/src/main/java/com/example/reto_1_2_sqlite/CalendarActivity.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/CalendarActivity.java
@@ -5,16 +5,37 @@ import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class CalendarActivity extends AppCompatActivity {
-    User user;
+    private User user;
+    private ArrayList<Visita> visitas;
+    private RecyclerView rclView;
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_pantalla_calendario);
 
+        //Obtenemos la referencia al RecyclerView
+        rclView = findViewById(R.id.rcVisitas);
+
+        //Recuperamos el User que ha iniciado sesión
         user = (User) getIntent().getSerializableExtra("cUser");
 
+        //Creamos el gestor para la conexión a la base de datos sqlite
+        DBHandler handler = new DBHandler(this);
 
+        //Recuperamos las visitas del usuario conectado de la base de datos
+        visitas = handler.getArrayVisitas(user);
+        handler.close();
+
+        //Creamos y establecemos el adaptador para la lista de visitas
+        VisitasAdapter adapter = new VisitasAdapter(visitas, this);
+        rclView.setAdapter(adapter);
+        rclView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
