18d295a5df9581708e7a30f08e843f32ae39f325
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-17 08:36:52 +0100

    Changed minimum SDK from 24 to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.reto_1_2_sqlite"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
