08e142219c9dca8cfab6eb526d432f54d64f1017
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-20 18:44:37 +0100

    Applied changes: - Added visit creation button - Added page refresh on back button action - Added user retrieval from intent

--- app/src/main/java/com/example/reto_1_2_sqlite/CalendarActivity.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/CalendarActivity.java
@@ -1,27 +1,33 @@
 package com.example.reto_1_2_sqlite;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class CalendarActivity extends AppCompatActivity {
-    private User user;
-    private ArrayList<Visita> visitas;
-    private RecyclerView rclView;
+public class CalendarActivity extends AppCompatActivity implements Serializable {
+    public User user;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_pantalla_calendario);
 
+        //Recuperamos la información del usuario registrado
+        user = (User) getIntent().getSerializableExtra("cUser");
+
         //Obtenemos la referencia al RecyclerView
-        rclView = findViewById(R.id.rcVisitas);
+        RecyclerView rclView = findViewById(R.id.rcVisitas);
 
         //Recuperamos el User que ha iniciado sesión
         user = (User) getIntent().getSerializableExtra("cUser");
@@ -30,12 +36,36 @@ public class CalendarActivity extends AppCompatActivity {
         DBHandler handler = new DBHandler(this);
 
         //Recuperamos las visitas del usuario conectado de la base de datos
-        visitas = handler.getArrayVisitas(user);
+        ArrayList<Visita> visitas = handler.getArrayVisitas(user);
         handler.close();
 
         //Creamos y establecemos el adaptador para la lista de visitas
         VisitasAdapter adapter = new VisitasAdapter(visitas, this);
         rclView.setAdapter(adapter);
         rclView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Acción de escucha para el botón de añadir visitas
+        FloatingActionButton btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(
+                        CalendarActivity.this,
+                        AnadirVisitas.class
+                );
+                myIntent.putExtra("cUser", user);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+    /*
+    Para refrescar la ventana cuando le damos al botón atrás en la barra de navegación del teléfono
+     */
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        finish();
+        startActivity(getIntent());
     }
 }
