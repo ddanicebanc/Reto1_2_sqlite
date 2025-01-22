57e26367e7550bf2a9c0135b0ddab21819f26d09
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-22 17:16:27 +0100

    Added ConsultaPedidos class to the project

---
+++ app/src/main/java/com/example/reto_1_2_sqlite/ConsultaPedidos.java
@@ -0,0 +1,9 @@
+package com.example.reto_1_2_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.Serializable;
+
+public class ConsultaPedidos extends AppCompatActivity implements Serializable {
+
+}
