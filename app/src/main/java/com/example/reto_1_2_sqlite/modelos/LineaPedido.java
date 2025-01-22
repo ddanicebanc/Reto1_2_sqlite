c20850afeb074b3d18c17aeb3d37bfe69c659d41
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-22 23:50:01 +0100

    Added LineaPedido class to the project

---
+++ app/src/main/java/com/example/reto_1_2_sqlite/modelos/LineaPedido.java
@@ -0,0 +1,54 @@
+package com.example.reto_1_2_sqlite.modelos;
+
+import java.io.Serializable;
+
+public class LineaPedido implements Serializable {
+    private int id, articuloId, cantidad, precio, cabPedidoId;
+
+    public LineaPedido( int articuloId, int cantidad, int precio, int cabPedidoId) {
+        this.articuloId = articuloId;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.cabPedidoId = cabPedidoId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getArticuloId() {
+        return articuloId;
+    }
+
+    public void setArticuloId(int articuloId) {
+        this.articuloId = articuloId;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCabPedidoId() {
+        return cabPedidoId;
+    }
+
+    public void setCabPedidoId(int cabPedidoId) {
+        this.cabPedidoId = cabPedidoId;
+    }
+}
