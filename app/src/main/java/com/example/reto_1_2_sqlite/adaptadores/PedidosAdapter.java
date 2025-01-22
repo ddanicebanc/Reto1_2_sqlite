431fbd0ba21febfa9dd71e313be3eccb192d1e30
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-22 23:51:41 +0100

    Finished PedidosAdapter adapter with missing methods from abstract class

--- app/src/main/java/com/example/reto_1_2_sqlite/adaptadores/PedidosAdapter.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/adaptadores/PedidosAdapter.java
@@ -1,11 +1,14 @@
 package com.example.reto_1_2_sqlite.adaptadores;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.reto_1_2_sqlite.R;
 import com.example.reto_1_2_sqlite.modelos.CabeceraPedido;
 
 import java.util.ArrayList;
@@ -27,7 +30,44 @@ public class PedidosAdapter extends RecyclerView.Adapter<PedidosAdapter.ViewHold
             super(view);
             view.setOnClickListener(this);
 
-            txvNumeroPedido = view.findViewById(R.id.)
+            txvNumeroPedido = view.findViewById(R.id.txvNumeroPedido);
+            txvFechaPedido = view.findViewById(R.id.txvFechaPedido);
         }
+
+        public TextView getTxvNumeroPedido () {return txvNumeroPedido;}
+        public TextView getTxvFechaPedido () {return txvFechaPedido;}
+
+        public void onClick (View view) {clickListener.onClick(view, getAdapterPosition());}
+    }
+
+    public PedidosAdapter (ArrayList<CabeceraPedido> dataSet, Context context) {
+        this.dataSet = dataSet;
+        this.context = context;
+    }
+
+    public void setOnClickListener (ItemClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder (ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.layout_cabeceras_pedido_row, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder (ViewHolder viewHolder, final int position) {
+        int numeroPedido = dataSet.get(position).getId();
+        String fechaPedido = dataSet.get(position).getFechaPedido();
+
+        viewHolder.getTxvNumeroPedido().setText(String.valueOf(numeroPedido));
+        viewHolder.getTxvFechaPedido().setText(fechaPedido);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
     }
 }
