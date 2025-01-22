68ea83f7bc7aeaa8d9a66ead6cbee448556c8b3f
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-22 23:53:54 +0100

    Added delegationId to the class attributes and constructor

--- app/src/main/java/com/example/reto_1_2_sqlite/modelos/User.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/modelos/User.java
@@ -4,11 +4,12 @@ import java.io.Serializable;
 
 public class User implements Serializable {
     private String name;
-    private int id;
+    private int id, delegationId;
 
-    public User (String name, int id) {
+    public User (String name, int id, int delegationId) {
         this.name = name;
         this.id = id;
+        this.delegationId = delegationId;
     }
 
     public int getId() {
@@ -26,4 +27,8 @@ public class User implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getDelegationId() { return delegationId;}
+
+    public void setDelegationId(int delegationId) {this.delegationId = delegationId;}
 }
