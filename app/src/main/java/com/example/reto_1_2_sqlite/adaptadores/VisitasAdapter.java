951f55d968b8b37508dc0d1226e6076228aa62fa
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-22 12:38:22 +0100

    Moved to adaptadores package

--- app/src/main/java/com/example/reto_1_2_sqlite/adaptadores/VisitasAdapter.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/adaptadores/VisitasAdapter.java
@@ -1,4 +1,4 @@
-package com.example.reto_1_2_sqlite;
+package com.example.reto_1_2_sqlite.adaptadores;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -7,6 +7,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.reto_1_2_sqlite.R;
+import com.example.reto_1_2_sqlite.modelos.Visita;
 
 import java.util.ArrayList;
 
