2c1a56bd2c8aa0517fed5d7aca57ee628794fce6
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-26 17:14:24 +0100

    Added basic class structure

--- app/src/main/java/com/example/reto_1_2_sqlite/consultas/ConsultaCatalogo.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/consultas/ConsultaCatalogo.java
@@ -1,4 +1,16 @@
 package com.example.reto_1_2_sqlite.consultas;
 
-public class ConsultaCatalogo {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.Serializable;
+
+public class ConsultaCatalogo extends AppCompatActivity implements Serializable {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
