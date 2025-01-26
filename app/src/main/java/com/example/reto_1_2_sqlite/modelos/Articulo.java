6351645b58353ca682f230fc59ad031acafe11f2
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-26 18:48:52 +0100

    Added nombre, imagen and constructor to Articulo

--- app/src/main/java/com/example/reto_1_2_sqlite/modelos/Articulo.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/modelos/Articulo.java
@@ -1,4 +1,29 @@
 package com.example.reto_1_2_sqlite.modelos;
 
+import android.graphics.Bitmap;
+
 public class Articulo {
+    private String nombre;
+    private Bitmap imagen;
+
+    public Articulo(String nombre, Bitmap imagen) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Bitmap getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Bitmap imagen) {
+        this.imagen = imagen;
+    }
 }
