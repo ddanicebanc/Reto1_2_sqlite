9d63d6922a314285bd8d08d675405cf14a3ddbfe
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-26 10:56:33 +0100

    Removed EdgeToEdge from layout

--- app/src/main/java/com/example/reto_1_2_sqlite/ConsultaPartners.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/ConsultaPartners.java
@@ -30,7 +30,6 @@ public class ConsultaPartners extends AppCompatActivity implements Serializable 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.layout_consulta_partners);
         Intent startIntent = getIntent();
         Bundle extras = startIntent.getExtras();
