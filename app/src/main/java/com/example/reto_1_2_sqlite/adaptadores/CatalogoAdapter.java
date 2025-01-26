17f748cc70e207a6e484fb24cf477342fa5b9dbc
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-01-26 18:49:28 +0100

    Finished CatalogoAdapter for the recycler view

--- app/src/main/java/com/example/reto_1_2_sqlite/adaptadores/CatalogoAdapter.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/adaptadores/CatalogoAdapter.java
@@ -1,4 +1,72 @@
 package com.example.reto_1_2_sqlite.adaptadores;
 
-public class CatalogoAdapter {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.reto_1_2_sqlite.R;
+import com.example.reto_1_2_sqlite.modelos.Articulo;
+
+import java.util.ArrayList;
+
+public class CatalogoAdapter extends RecyclerView.Adapter<CatalogoAdapter.ViewHolder> {
+    public interface ItemClickListener {
+        public void onClick (View view, int position);
+    }
+
+    private ArrayList<Articulo> dataSet;
+    private CatalogoAdapter.ItemClickListener clickListener;
+    private Context context;
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final TextView txvNombreArticulo;
+        private final ImageView imgArticulo;
+
+        public ViewHolder (View view) {
+            super(view);
+            view.setOnClickListener(this);
+
+            txvNombreArticulo = view.findViewById(R.id.txvNombreArticulo);
+            imgArticulo = view.findViewById(R.id.imgCatalogo);
+        }
+
+        public TextView getTxvNombreArticulo () {return txvNombreArticulo;}
+        public ImageView getImgArticulo () {return imgArticulo;}
+
+        public void onClick (View view) {clickListener.onClick(view, getAdapterPosition());}
+    }
+
+    public CatalogoAdapter (ArrayList<Articulo> dataSet, Context context) {
+        this.dataSet = dataSet;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_catalogo, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder (ViewHolder viewHolder, final int position) {
+        String nombre = dataSet.get(position).getNombre();
+        Bitmap imagenArticulo = dataSet.get(position).getImagen();
+
+        viewHolder.getTxvNombreArticulo().setText(nombre);
+        viewHolder.getImgArticulo().setImageBitmap(imagenArticulo);
+    }
+
+    @Override
+    public int getItemCount () {return dataSet.size();}
 }
