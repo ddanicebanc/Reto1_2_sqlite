dafd6ad99fb652bafbbabfa4588e63f53490a728
Dani Duarte <145504727+ddanicebanc@users.noreply.github.com>
2025-02-02 17:12:09 +0100

    Added footer and footer buttons listeners

--- app/src/main/java/com/example/reto_1_2_sqlite/consultas/ConsultaCatalogo.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/consultas/ConsultaCatalogo.java
@@ -3,12 +3,14 @@ package com.example.reto_1_2_sqlite.consultas;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.reto_1_2_sqlite.PantallaPrincipal;
 import com.example.reto_1_2_sqlite.conexiones.DBHandler;
 import com.example.reto_1_2_sqlite.R;
 import com.example.reto_1_2_sqlite.adaptadores.CatalogoAdapter;
@@ -39,6 +41,67 @@ public class ConsultaCatalogo extends AppCompatActivity implements Serializable,
                 LinearLayoutManager.HORIZONTAL,
                 false));
         adapter.setOnClickListener(this::onClick);
+
+        //Funcionamiento de los botones del footer
+        ImageButton btnHome = findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaCatalogo.this,
+                        PantallaPrincipal.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        ImageButton btnCal = findViewById(R.id.btnAg);
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaCatalogo.this,
+                        ConsultaVisitas.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        ImageButton btnPar = findViewById(R.id.btnPar);
+        btnPar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaCatalogo.this,
+                        ConsultaPartners.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        ImageButton btnPed = findViewById(R.id.btnPed);
+        btnPed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaCatalogo.this,
+                        ConsultaPedidos.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/reto_1_2_sqlite/consultas/ConsultaPedidos.java
+++ app/src/main/java/com/example/reto_1_2_sqlite/consultas/ConsultaPedidos.java
@@ -3,12 +3,14 @@ package com.example.reto_1_2_sqlite.consultas;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.reto_1_2_sqlite.PantallaPrincipal;
 import com.example.reto_1_2_sqlite.anadir.AnadirCabeceraPedido;
 import com.example.reto_1_2_sqlite.conexiones.DBHandler;
 import com.example.reto_1_2_sqlite.R;
@@ -55,6 +57,67 @@ public class ConsultaPedidos extends AppCompatActivity implements Serializable {
                 );
                 intent.putExtra("cUser", user);
                 startActivity(intent);
+            }
+        });
+
+        //Funcionamiento de los botones del footer
+        ImageButton btnHome = findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaPedidos.this,
+                        PantallaPrincipal.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        ImageButton btnCal = findViewById(R.id.btnAg);
+        btnCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaPedidos.this,
+                        ConsultaVisitas.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        ImageButton btnCat = findViewById(R.id.btnCat);
+        btnCat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaPedidos.this,
+                        ConsultaCatalogo.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        ImageButton btnPar = findViewById(R.id.btnPar);
+        btnPar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        ConsultaPedidos.this,
+                        ConsultaPartners.class
+                );
+                i.putExtra("cUser", user);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
             }
         });
     }
